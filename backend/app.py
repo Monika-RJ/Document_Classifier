628f3aa65dfe5edbb436a04b9916e488b8116eb0
RJ-MONIKA <monikarj.22aid@kongu.edu>
2025-02-13 22:20:21 +0530

    Add

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 import joblib
 from flask_cors import CORS
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)  # This allows cross-origin requests from React frontend
 
 # Load the trained model and vectorizer
-model = joblib.load('documentclassification/backend/models/logistic_regression_model.pkl')
-vectorizer = joblib.load('documentclassification/backend/models/tfidf_vectorizer.pkl')
+model_path = os.path.join(os.path.dirname(__file__), 'models', 'logistic_regression_model.pkl')
+vectorizer_path = os.path.join(os.path.dirname(__file__), 'models', 'tfidf_vectorizer.pkl')
+
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
 
 # Prediction route
 @app.route('/predict', methods=['POST'])
